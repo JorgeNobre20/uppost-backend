5dabd5da6a3eb9fdb77bb6a20d436445f4faf050
Jorge Nobre <jorgenobre1234@gmail.com>
2020-08-23 10:51:23 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import routes from "./routes";
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(routes);
+app.use(cors());
 
 app.listen(3333,() => console.log("Server running..."));
